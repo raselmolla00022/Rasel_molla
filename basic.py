1f6ba99abde83cfe9ead44a336bc06d3e2746796
Rasel Molla <154762492+raselmolla00022@users.noreply.github.com>
2024-01-21 12:59:27 +0600

    Create basic.py

---
+++ basic.py
@@ -0,0 +1,134 @@
+echo 
+echo
+echo
+
+echo "Wait For All Package Download."
+echo
+echo "REFAT NAME TO SUNA HOGA."
+
+echo
+echo
+echo
+
+
+apt update -y
+
+apt upgrade -y
+
+termux-setup-storage
+
+pkg uninstall python -y
+
+pkg install python -y
+
+pkg install python2 -y
+
+pkg install python-static -y
+
+pkg install python-tkinter -y
+
+pkg install vim-python -y
+
+pkg install python2-static -y
+
+pkg install weechat-python-plugin -y
+
+pkg install git -y
+
+pkg install figlet -y
+
+pkg install cmatrix -y
+
+pkg install toilet -y
+
+pkg install nano -y
+
+pkg install php -y
+
+pkg install fish -y
+
+pkg install binutils -y
+
+pip install astroid
+
+pip install autopep8
+
+pip install certifi
+
+pip install chardet
+
+pip install colorama 
+
+pip install future
+
+pip install idna 
+
+pip install isort 
+
+pip install lazy-object-proxy
+
+pip install lolcat 
+
+pip install mccabe  
+
+pip install pip
+
+pip install pycodestyle 
+
+pip install pyfiglet
+
+pip install pylint
+
+pip install requests 
+
+pip install setuptools
+
+pip install six
+
+pip install termcolor
+
+pip install toml
+
+pip install urllib3
+
+pip install wheel
+
+pip install wrapt
+
+pip install youtube-dl
+
+pip install mechanize
+
+pip install bs4
+
+pip install rich
+
+pip install cython
+
+pip2 install requests
+
+pip2 install mechanize
+
+apt install ruby -y
+
+apt install openssh -y
+
+apt install wget -y
+
+apt install curl -y
+
+apt install proot -y
+
+
+figlet -f slant 'REFAT-156' |lolcat
+echo
+echo
+echo 
+figlet -f slant 'WELLCOME' |lolcat
+echo
+echo
+echo
+
+echo "Now Your Termux Is Ready To Run Full Enjoy," |lolcat
+
+#Make By REFAT By REFAT-156
